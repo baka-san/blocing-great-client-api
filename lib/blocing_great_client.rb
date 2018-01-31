103f5d53fda20f45cae59e06472e341c6d207f73
Grant <gsbackes@gmail.com>
2018-02-01 01:01:29 +0900

    Checkpoint 3: check mentor availability

--- lib/blocing_great_client.rb
+++ lib/blocing_great_client.rb
@@ -24,14 +24,39 @@ class BlocingGreatClient
       content_type: 'application/json',
       authorization: @auth_token
     }
-    options = {headers: headers}
+    options = { headers: headers }
     response = self.class.get('/users/me', options)
+    response_json = JSON.parse(response.body)
+  end
+
+  # Retrieve mentor availability
+  def get_mentor_availability
+    headers = {
+      content_type: 'application/json',
+      authorization: @auth_token
+    }
+
+    current_user = self.get_me
+    student_id = current_user['id']
+    mentor_id = current_user['current_enrollment']['mentor_id']
+
+    options = { headers: headers}
+    url = "/mentors/#{mentor_id}/student_availability"
+    response = self.class.get(url, options)
     response_json = JSON.parse(response.body)
   end
 
 end
 
 # self.class.post?
+
 # auth = { email: email, password: password }
 # options = { basic_auth: auth }
 # response = self.class.get('/sessions', options)
+
+
+# student_id = self.get_me[:id] or .id
+
+
+
+
